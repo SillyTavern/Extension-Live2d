9eae7ac37a3742d19352d44604e7cca7f3a7b123
Tony Ribeiro <tonyribeiro.research.aca@gmail.com>
2023-10-18 04:30:28 +0200

    Added frame debug option and model scaling slider.

--- index.js
+++ index.js
@@ -7,19 +7,21 @@ TODO:
   - Background transparent OK
   - Load character model from character folder OK
   - Update character model when chat change OK
-  - Resize model option
+  - Resize model option OK
   - Automatically load hit frames and animation list
   - UI for user to connect hit frames with animation
   - Plug to expression
   - Play mouth animation when talking (message length dependant)
   - model setting per character
   - Group chat mode
+  - Allow multiple models for one character / ability to switch / force animation
+  - UI mapping ST expression to live2d expression+motion
+  - activate/disable hitframes message + auto-send
 */
 
 import { loadFileToDocument } from "../../../utils.js";
-import { saveSettingsDebounced, getRequestHeaders, eventSource, event_types } from "../../../../script.js";
+import { saveSettingsDebounced, getRequestHeaders, eventSource, event_types, sendMessageAsUser } from "../../../../script.js";
 import { getContext, extension_settings, ModuleWorkerWrapper } from "../../../extensions.js";
-import { getMessageTimeStamp } from "../../../RossAscends-mods.js";
 export { MODULE_NAME };
 
 const extensionFolderPath = `scripts/extensions/third-party/Extension-Live2d`;
@@ -56,9 +58,16 @@ let app = null;
 //#############################//
 
 const defaultSettings = {
+    // Global settings
     enabled: false,
-    showHitFrames: false,
     followCursor: false,
+    autoSendInteraction: false,
+
+    // Model settings
+    modelScale: 1.0,
+
+    // Debug
+    showFrames: false,
 }
 
 function loadSettings() {
@@ -72,7 +81,12 @@ function loadSettings() {
 
     $("#live2d_enabled").prop('checked', extension_settings.live2d.enabled);
     $("#live2d_follow_cursor").prop('checked', extension_settings.live2d.followCursor);
-    $("#live2d_show_hit_frames").prop('checked', extension_settings.live2d.showHitFrames);
+    $("#live2d_auto_send_interaction").prop('checked', extension_settings.live2d.autoSendInteraction);
+
+    $("#live2d_model_scale").val(extension_settings.live2d.modelScale);
+    $("#live2d_model_scale_value").text(extension_settings.live2d.modelScale);
+    
+    $("#live2d_show_frames").prop('checked', extension_settings.live2d.showFrames);
 }
 
 async function onEnabledClick() {
@@ -84,21 +98,37 @@ async function onFollowCursorClick() {
   extension_settings.live2d.followCursor = $('#live2d_follow_cursor').is(':checked');
   saveSettingsDebounced();
 
-  models.forEach((model) => {
-    model._autoInteract = extension_settings.live2d.followCursor;
-  });
   loadLive2d();
 }
 
-async function onShowHitFramesClick() {
-  extension_settings.live2d.showHitFrames = $('#live2d_show_hit_frames').is(':checked');
+async function onAutoSendInteractionClick() {
+  extension_settings.live2d.autoSendInteraction = $('#live2d_auto_send_interaction').is(':checked');
   saveSettingsDebounced();
-
-  models.forEach((model) => {
-    model.children[0].visible = extension_settings.live2d.showHitFrames;
-  });
 }
 
+async function onModelScaleChange() {
+  extension_settings.live2d.modelScale = Number($('#live2d_model_scale').val());
+  $("#live2d_model_scale_value").text(extension_settings.live2d.modelScale);
+  saveSettingsDebounced();
+  loadLive2d();
+}
+
+async function onShowFramesClick() {
+  extension_settings.live2d.showFrames = $('#live2d_show_frames').is(':checked');
+  saveSettingsDebounced();
+  loadLive2d();
+}
+
+
+async function onHitAreasClick(hitAreas) {
+  $('#send_textarea').val("") // clear message area to avoid double message
+  console.debug(DEBUG_PREFIX,"Detected click on hit areas:", hitAreas);
+
+  //sendMessageAsUser(text);
+  
+  // TODO: auto-send option
+  //getContext().generate();
+}
 
 //#############################//
 //  Methods                    //
@@ -121,28 +151,18 @@ function draggable(model) {
   model.on("pointerup", () => (model.dragging = false));
 }
 
-function addHitAreaFrames(model) {
+function showFrames(model) {
+  const foreground = PIXI.Sprite.from(PIXI.Texture.WHITE);
+  foreground.width = model.internalModel.width;
+  foreground.height = model.internalModel.height;
+  foreground.alpha = 0.2;
+  foreground.visible = true;
+
   const hitAreaFrames = new live2d.HitAreaFrames();
+  hitAreaFrames.visible = true;
 
+  model.addChild(foreground);
   model.addChild(hitAreaFrames);
-  hitAreaFrames.visible = extension_settings.live2d.showHitFrames;
-}
-
-async function sendMessage(text) {
-  $('#send_textarea').val("") // clear message area to avoid double message
-  console.debug(DEBUG_PREFIX,"Sending message head pat")
-  const context = getContext();
-  const messageText = text;
-  const message = {
-      name: context.name1,
-      is_user: true,
-      send_date: getMessageTimeStamp(),
-      mes: messageText,
-  };
-  context.chat.push(message);
-  context.addOneMessage(message);
-
-  context.generate();
 }
 
 async function loadLive2d() {
@@ -158,7 +178,8 @@ async function loadLive2d() {
 
   document.getElementById("live2d-canvas").hidden = false;
 
-  const model = await live2d.Live2DModel.from(characters_model_path[character]);// TODO: multiple models
+  let model = await live2d.Live2DModel.from(characters_model_path[character]);// TODO: multiple models
+  models = [model];
 
   app = new PIXI.Application({
     view: document.getElementById("live2d-canvas"),
@@ -169,8 +190,10 @@ async function loadLive2d() {
 
   app.stage.addChild(model);
 
-  const scaleX = (innerWidth * 0.4) / model.width;
-  const scaleY = (innerHeight * 0.8) / model.height;
+  console.debug(DEBUG_PREFIX,innerWidth, " ", innerHeight)
+
+  const scaleX = ((innerWidth) / model.width) * extension_settings.live2d.modelScale;
+  const scaleY = ((innerHeight) / model.height) * extension_settings.live2d.modelScale;
 
   // fit the window
   model.scale.set(Math.min(scaleX, scaleY));
@@ -179,11 +202,18 @@ async function loadLive2d() {
   model.y = innerHeight * 0.1;
 
   draggable(model);
-  //addFrame(model);
-  addHitAreaFrames(model);
+
+  if (extension_settings.live2d.showFrames)
+    showFrames(model);
+
+  // DBG
+  model.on("click", () => {model.expression();model.internalModel.motionManager.stopAllMotions(); model.motion("")}); // check stop motion
 
   // handle tapping
+  model.on("hit", (hitAreas) => onHitAreasClick(hitAreas));
+  /*
   model.on("hit", (hitAreas) => {
+
     if (hitAreas.includes("body")) {
       model.motion("tap_body");
 
@@ -204,11 +234,13 @@ async function loadLive2d() {
       }
     }
   });
+  */
 
   // Set cursor behavior
   model._autoInteract = extension_settings.live2d.followCursor;
-  console.debug(DEBUG_PREFIX,model);
+  model.children[0].visible = extension_settings.live2d.showHitFrames;
 
+  console.debug(DEBUG_PREFIX,model);
   
 }
 
@@ -260,6 +292,7 @@ async function moduleWorker() {
     const moduleEnabled = extension_settings.live2d.enabled;
 
     if (moduleEnabled) {
+      // DBG
 
     }
 }
@@ -278,7 +311,10 @@ jQuery(async () => {
     // Set user interactions
     $("#live2d_enabled").on("click", onEnabledClick);
     $("#live2d_follow_cursor").on("click", onFollowCursorClick);
-    $("#live2d_show_hit_frames").on("click", onShowHitFramesClick);
+
+    $("#live2d_model_scale").on("input", onModelScaleChange);
+
+    $("#live2d_show_frames").on("click", onShowFramesClick);
     
     // Module worker
     const wrapper = new ModuleWorkerWrapper(moduleWorker);
